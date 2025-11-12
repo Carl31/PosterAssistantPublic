8fa545db3820f771bd86b9b131dfa761b7990bc3
Carl1131 <carlsantos31@yahoo.com>
2025-11-13 12:50:37 +1300

    small fix

--- frontend/src/app/generate/upload/page.tsx
+++ frontend/src/app/generate/upload/page.tsx
@@ -19,6 +19,7 @@ import { getAuth, onAuthStateChanged, User } from 'firebase/auth'
 import LoadingPage from '@/components/LoadingPage'
 import Spinner from '@/components/Spinner'
 import { Archivo_Black } from 'next/font/google'
+import Notification from '@/components/Notification'
 
 const archivoBlack = Archivo_Black({
   weight: '400',
