c60917a7fd2f4067b204098e65d0839b77fa9500
Carl1131 <carlsantos31@yahoo.com>
2025-11-30 12:30:48 +1300

    recorrect wrong change

--- frontend/src/app/account/dashboard/page.tsx
+++ frontend/src/app/account/dashboard/page.tsx
@@ -48,7 +48,6 @@ export default function DashboardPage() {
 
     console.log("Credits:", credits)
     if (credits.posterGen <= 0) {
-router.push('/generate/upload')
       notify('error', 'You have no credits left.')
       return
     } else if (credits.posterGen > 0) {
